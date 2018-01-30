425719ead6607bfc7af183fc578e257ed15b88de
Anders Kvist <ak@cego.dk>
2018-01-30 21:53:56 +0100

    Simple script for logging DVI heat pump data to influx

---
+++ quick.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+COOKIE=${1}
+
+while read LINE; do
+
+  K=$(echo ${LINE}|awk -F'[ "]' '{print $4}')
+  V=$(echo ${LINE}|awk -F'[>&]' '{print $2}')
+
+
+  case "${K}" in
+    value7)
+      echo dvienergi.smartcontrol.outside value=${V}0
+      ;;
+    value5)
+      echo dvienergi.smartcontrol.heating value=${V}0
+      ;;
+    value3)
+      echo dvienergi.smartcontrol.hotwater value=${V}0
+      ;;
+    value1)
+      echo dvienergi.smartcontrol.heating_forward value=${V}0
+      ;;
+    value2)
+      echo dvienergi.smartcontrol.heating_return1 value=${V}0
+      ;;
+    value8)
+      echo dvienergi.smartcontrol.heating_return2 value=${V}0
+      ;;
+    value13)
+      echo dvienergi.smartcontrol.ground_return value=${V}0
+      ;;
+    value14)
+      echo dvienergi.smartcontrol.ground_forward value=${V}0
+      ;;
+    value12)
+      echo dvienergi.smartcontrol.compressor_front value=${V}0
+      ;;
+    value11)
+      echo dvienergi.smartcontrol.compressor_back value=${V}0
+      ;;
+    *)
+      echo ${LINE} 1>&2
+      echo ${K}: ${V} 1>&2
+      ;;
+
+  esac
+done < <(curl 'https://smartcontrol.dvienergi.com/includes/process.php' -H "cookie: PHPSESSID=${COOKIE}" --data 'subupdatepumpgraphics=1' -s|sed 's/></>\n</g'|grep "temp value")
