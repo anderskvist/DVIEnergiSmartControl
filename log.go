4c7632beef6ebbb487f82932a82e8a24b52b2dd0
Anders Kvist <ak@cego.dk>
2019-05-24 09:45:53 +0200

    Added better logging - starting with a full example for later adjustment.

---
+++ log.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "os"
+  logging "github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("example")
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+func init () {
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+
+	// Only errors and more severe messages should be sent to backend1
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.ERROR, "")
+
+	// Set the backends to be used.
+	logging.SetBackend(backend1Leveled, backend2Formatter)
+}
